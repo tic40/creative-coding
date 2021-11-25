a85eca5309354f3c9d2b762acc025ceeb5af1141
tic40 <ccpzjoh@gmail.com>
2021-11-26 01:31:55 +0900

    Add linear_interpolation

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,24 +8,38 @@ const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>tic40/creative-coding</title>
+        <title>
+          tic40/creative-coding</title>
         <meta name="description" content="tic40/creative-coding" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="m-4">
-        <h1 className="text-4xl">tic40/creative-coding</h1>
+        <h1 className="text-4xl">
+          <Link href="/">
+            <a>tic40/creative-coding</a>
+          </Link>
+        </h1>
         <ul className="my-4">
-        {(sketchFileNames.map((name: string) => {
-          const sketch = require(`../sketches/${name}`)
-          return (
-            <li key={`sketch-${name}`} className="underline text-xl">
-              <Link href={`/sketches/${name}`}>
-                <a> {name} </a>
-              </Link>
-            </li>
-          )
-        }))}
+          {(sketchFileNames.map((name: string) => {
+            const sketch = require(`../sketches/${name}`)
+            return (
+              <li key={`sketch-${name}`}>
+                <Link href={`/sketches/${name}`}>
+                  <a className="link text-xl"> {name} </a>
+                </Link>
+              </li>
+            )
+          }))}
         </ul>
+        <footer>
+          <a
+            href="https://github.com/tic40/creative-coding"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
       </main>
     </>
   )
