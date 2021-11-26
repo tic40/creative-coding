46c31c107a9240a5454b7f1cc9470b446a962428
tic40 <ccpzjoh@gmail.com>
2021-11-26 11:34:56 +0000

    Deploying to gh-pages from @ tic40/creative-coding@99cc0c25fefb8af1a8863fc1f7548f5cd40ab6e0 🚀

--- _next/static/3aFvZprtb2RJZiLTggdhJ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-0abfd1fe81ade78e.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],"/sketches/[name]":["static/chunks/pages/sketches/[name]-9f195fd53e431aa9.js"],sortedPages:["/","/_app","/_error","/sketches/[name]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/81jq1pRsDsls3bff5NwQZ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-5e6c4b2b96ecff27.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],"/sketches/[name]":["static/chunks/pages/sketches/[name]-58b886f76b3064ab.js"],sortedPages:["/","/_app","/_error","/sketches/[name]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
