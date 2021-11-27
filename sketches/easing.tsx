0563ad87639eb94cea7658452482747c9a81fbc9
tic40 <ccpzjoh@gmail.com>
2021-11-27 16:27:00 +0900

    Update easing

--- sketches/easing.tsx
+++ sketches/easing.tsx
@@ -1,15 +1,7 @@
 import p5Types from 'p5'
 import { easingsFunctions, easingsFunctionNames } from '../utils/easings'
 
-interface Route {
-  x: number,
-  y: number
-}
-
-let route: Route[]
 let x = 0
-let y = 0
-let rev = false
 let top = 0
 let bottom = 0
 let prev = 0
@@ -23,14 +15,13 @@ export function setup(p: p5Types, canvasParentRef: Element) {
   top = p.height - p.height / 10
   bottom = p.height / 10
   x = p.width / 2
-  y = bottom
   prev = bottom
   next = top
   setRandomEasingsFunctions()
 }
 
 export function draw(p: p5Types) {
-  // 誤差がでるのでtは100倍の値を入れておく
+  // 誤差がでるのでtはtは100倍の値を入れておく
   t += 1
 
   if (t >= 140) {
@@ -45,15 +36,17 @@ export function draw(p: p5Types) {
 
   p.clear()
 
-  p.textSize(50)
-  p.fill(100)
-  p.stroke(100)
-  p.text(currentEasingName, 10, p.height/2)
+  p.strokeWeight(0)
+  p.textSize(16)
+  easingsFunctionNames.forEach((v,i) => {
+    if (v == currentEasingName) p.fill("#FF0000")
+    else p.fill(150)
+    p.text(v, 16, p.height/10 + i*16)
+  })
 
   p.strokeWeight(5)
   p.stroke(0)
   p.line(x, top, x, bottom)
-
 
   p.stroke("#FF0000")
   const d = p.lerp(prev, next, easingsFunctions[currentEasingName](t/100))
