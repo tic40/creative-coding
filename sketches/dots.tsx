23040859f03cd53c37d8d77a67a3d12ae9871fa9
tic40 <ccpzjoh@gmail.com>
2021-11-30 01:20:10 +0900

    Update dots

--- sketches/dots.tsx
+++ sketches/dots.tsx
@@ -1,9 +1,7 @@
 import p5Types from 'p5'
 
-const maxN = 30
-let n = 10
+const n = 20
 let t = 0
-let rev = false
 
 export function setup(p: p5Types, canvasParentRef: Element) {
   p.createCanvas(p.windowWidth, p.windowHeight).parent(canvasParentRef)
@@ -12,30 +10,21 @@ export function setup(p: p5Types, canvasParentRef: Element) {
 
 export function draw(p: p5Types) {
   t++
-  t %= 10
+  t %= p.height / (n - 1)
   p.clear()
   p.background(0)
   const maxDist = p.dist(0, 0, p.width / 2, p.height / 2);
 
-  for (let y = -n*10; y < n; y++) {
-    for (let x = -n*10; x < n; x++) {
+  for (let y = 0; y < n; y++) {
+    for (let x = 0; x < n; x++) {
       const tx = (p.width / (n - 1)) * x
       const ty = (p.height / (n - 1)) * y
 
       const d = p.dist(tx, ty, p.mouseX, p.mouseY)
       const h = p.map(d, 0, maxDist, 0, 360);
       p.fill(h, 255, 255);
-      if (rev) {
-        p.circle(tx, ty-t*10, 10)
-      } else {
-        p.circle(tx, ty+t*10, 10)
-      }
+      p.noStroke()
+      p.circle(tx, ty+t, 10)
     }
   }
-}
-
-export function mouseClicked(p: p5Types) {
-  rev = !rev
-  n += 10
-  if (maxN < n) n = 10
 }
