9df8e3a12e926d644ab63e2ac9ccca7077602fa3
tic40 <ccpzjoh@gmail.com>
2021-12-09 12:06:25 +0000

    Deploying to gh-pages from @ tic40/creative-coding@c226bca8af70de9bbd25f9bab0a209db2eb8c28f 🚀

---
+++ _next/static/500m8hDQSkCg-0w7KGNSr/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-1deba826218abf12.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],"/sketches/[name]":["static/chunks/pages/sketches/[name]-39f967fe7412f775.js"],sortedPages:["/","/_app","/_error","/sketches/[name]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
