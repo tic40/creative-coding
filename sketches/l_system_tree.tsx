bb33b7e006f9c8c52b9d11bb2ea2b5f5e0df6b80
tic40 <ccpzjoh@gmail.com>
2022-04-22 22:13:52 +0900

    Update

--- sketches/l_system_tree.tsx
+++ sketches/l_system_tree.tsx
@@ -1,9 +1,8 @@
 import p5Types from 'p5'
 
-type Command = 'F' | 'X' | '-' | '[' | ']'
 interface Data {
-  start: Command
-  rules: { X: string; F: string }
+  start: string
+  rules: Record<string, unknown>
   length: number
   depth: number
   x: number
@@ -12,27 +11,87 @@ interface Data {
   angle: number
 }
 
-let p: p5Types
-let drawData: { x: number; y: number; tx: number; ty: number }[]
+class LSystem {
+  p: p5Types
+  data: Data
+  command: string
+  drawData: { x: number; y: number; tx: number; ty: number }[]
+  stack: { angle: number; x: number; y: number }[]
 
-function addCommand(
-  command: string,
-  rules: Data['rules'],
-  i: number,
-  depth: number
-): string {
-  if (depth <= i) return command
-  const cmd = Array.from(command)
-    .map((k) => {
-      if (k == 'X' || k == 'F') return rules[k]
-      return k
-    })
-    .join('')
-  return addCommand(cmd, rules, i + 1, depth)
+  constructor(d: Data, p: p5Types) {
+    this.data = d
+    this.p = p
+    this.drawData = []
+    this.stack = []
+    this.command = this.generateCommand(this.data.start, 0)
+    this.prepare()
+  }
+
+  prepare() {
+    let angle = this.data.startAngle
+    let x = this.data.x
+    let y = this.data.y
+    const p = this.p
+
+    for (const c of this.command) {
+      switch (c) {
+        case '+':
+          angle -= this.data.angle
+          break
+        case '-':
+          angle += this.data.angle
+          break
+        case '[':
+          this.stack.push({ angle, x, y })
+          break
+        case ']':
+          const d = this.stack.pop()
+          if (d) [angle, x, y] = [d.angle, d.x, d.y]
+          break
+        case 'F':
+          const tx = x + p.cos(angle) * this.data['length']
+          const ty = y + p.sin(angle) * this.data['length']
+          this.drawData.push({ x, y, tx, ty })
+          x = tx
+          y = ty
+          break
+        case 'X':
+          // do nothing
+          break
+        default:
+          console.error(`illegal command: ${c}`)
+          break
+      }
+    }
+  }
+
+  generateCommand(command: string, i: number): string {
+    const d = this.data
+    if (d.depth <= i) return command
+
+    const cmd = Array.from(command)
+      .map((k) => d.rules[k] || k)
+      .join('')
+    return this.generateCommand(cmd, i + 1)
+  }
+
+  draw(steps: number): void {
+    const p = this.p
+    p.stroke(0)
+    p.noFill()
+
+    for (let i = 0; i < steps; i++) {
+      if (this.drawData.length == 0) return
+      const d = this.drawData.shift()
+      if (d) p.line(d.x, d.y, d.tx, d.ty)
+    }
+  }
 }
 
-function createData(): Data {
-  // TODO: 他のバリエーション
+let lSystem: LSystem
+
+function init(p: p5Types) {
+  // TODO: データ生成パターン
   const { length, depth } = p.random([
     { length: 1, depth: 8 },
     { length: 2, depth: 7 },
@@ -43,10 +102,9 @@ function createData(): Data {
     { length: 5, depth: 5 },
   ])
 
-  const r = ['F-[[X]+X]+F[+FX]-X']
-  return {
+  const data = {
     start: 'X',
-    rules: { X: r[0], F: 'FF' },
+    rules: { X: 'F-[[X]+X]+F[+FX]-X', F: 'FF' },
     length: length,
     depth: depth,
     x: p.width / 2,
@@ -54,81 +112,29 @@ function createData(): Data {
     startAngle: -90,
     angle: p.random(20, 25),
   }
+
+  lSystem = new LSystem(data, p)
 }
 
-function init() {
-  const data = createData()
-  console.log('data', data)
-  drawData = []
-  const command = addCommand(data.start, data.rules, 0, data.depth)
-
-  const stack: { angle: number; x: number; y: number }[] = []
-  let angle = data.startAngle
-  let x = data.x
-  let y = data.y
-  for (const c of command) {
-    switch (c) {
-      case '+':
-        angle -= data.angle
-        break
-      case '-':
-        angle += data.angle
-        break
-      case '[':
-        stack.push({ angle, x, y })
-        break
-      case ']':
-        const d = stack.pop()
-        if (d) [angle, x, y] = [d.angle, d.x, d.y]
-        break
-      case 'F':
-        const tx = x + (p.cos(angle) * data.length)
-        const ty = y + (p.sin(angle) * data.length)
-        drawData.push({ x, y, tx, ty })
-        x = tx
-        y = ty
-        break
-      case 'X':
-        // do nothing
-        break
-      default:
-        console.error(`illegal command: ${c}`)
-        break
-    }
-  }
-}
-
-export function setup(_p: p5Types, canvasParentRef: Element) {
-  p = _p
+export function setup(p: p5Types, canvasParentRef: Element) {
   p.createCanvas(p.windowWidth, p.windowHeight).parent(canvasParentRef)
   p.angleMode(p.DEGREES)
-
-  init()
+  init(p)
 }
 
 export function draw(p: p5Types) {
   p.stroke(0)
   p.noFill()
 
-  console.log(drawData.length)
-
-  const len = drawData.length
-  let speed = 10
-  if (10000 < len) speed = 500
-  else if (5000 < len) speed = 100
-  else if (1000 < len) speed = 50
-
-  for (let i = 0; i < speed; i++) {
-    if (drawData.length == 0) return
-    const d = drawData.shift()
-    if (d) {
-      const { x, y, tx, ty } = d
-      p.line(x, y, tx, ty)
-    }
-  }
+  const len = lSystem.drawData.length
+  let steps = 10
+  if (10000 < len) steps = 500
+  else if (5000 < len) steps = 100
+  else if (1000 < len) steps = 50
+  lSystem.draw(steps)
 }
 
-export function mouseClicked() {
+export function mouseClicked(p: p5Types) {
   p.clear()
-  init()
+  init(p)
 }
