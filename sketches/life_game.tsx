2611a39aadb6530fdc6b094231d636c0169f48e0
tic40 <ccpzjoh@gmail.com>
2022-04-27 09:42:01 +0900

    Update

--- sketches/life_game.tsx
+++ sketches/life_game.tsx
@@ -5,11 +5,56 @@ const CellType = { dead: 0, alive: 1 } as const
 type valueOf<T> = T[keyof T]
 type Board = valueOf<typeof CellType>[][]
 const cellSize = 10
+const buttonStyle =
+  'background: #4CAF50; opacity: 0.8; color: white; padding: 8px; border: none;'
+
 let cellWidth: number
 let cellHeight: number
 let board: Board
 let drawBoard: Board
 let speed = 0
+let pause = 1
+
+export function setup(p: p5Types, canvasParentRef: Element) {
+  p.createCanvas(p.windowWidth, p.windowHeight).parent(canvasParentRef)
+
+  const startBtn = p.createButton('Start')
+  startBtn.position(0, 0)
+  startBtn.mousePressed(start)
+  startBtn.style(buttonStyle)
+
+  const stopBtn = p.createButton('Stop')
+  stopBtn.position(0, 50)
+  stopBtn.mousePressed(stop)
+  stopBtn.style(buttonStyle)
+
+  const clearBtn = p.createButton('Clear')
+  clearBtn.position(0, 100)
+  clearBtn.mousePressed(() => initBoard(p))
+  clearBtn.style(buttonStyle)
+
+  const randomBtn = p.createButton('Random')
+  randomBtn.position(0, 150)
+  randomBtn.mousePressed(() => randomBoard(p))
+  randomBtn.style(buttonStyle)
+
+  const changeSpeedBtn = p.createButton('Change speed')
+  changeSpeedBtn.position(0, 200)
+  changeSpeedBtn.mousePressed(toggleSpeed)
+  changeSpeedBtn.style(buttonStyle)
+
+  initBoard(p)
+  randomBoard(p)
+  drawCurrentBoard(p)
+}
+
+export function draw(p: p5Types) {
+  if (pause) return
+  if (speed == 1 && p.frameCount % 10) return
+  else if (p.frameCount % 2) return
+
+  proceed(p)
+}
 
 function calcAliveCellNum(y: number, x: number): number {
   let res = 0
@@ -26,28 +71,7 @@ function calcAliveCellNum(y: number, x: number): number {
   return res
 }
 
-export function setup(p: p5Types, canvasParentRef: Element) {
-  p.createCanvas(p.windowWidth, p.windowHeight).parent(canvasParentRef)
-
-  cellHeight = p.round(p.height / cellSize)
-  cellWidth = p.round(p.width / cellSize)
-  board = new Array(cellHeight).fill(0).map(() => new Array(cellWidth).fill(0))
-  drawBoard = board.map((v) => [...v])
-
-  for (let i = 0; i < cellHeight; i++) {
-    for (let j = 0; j < cellWidth; j++) {
-      const t = p.random(100) > 60 ? CellType.alive : CellType.dead
-      board[i][j] = drawBoard[i][j] = t
-    }
-  }
-
-  p.stroke(Color.stroke)
-}
-
-export function draw(p: p5Types) {
-  if (speed == 1 && p.frameCount % 5) return
-  else p.frameCount % 2
-
+function proceed(p: p5Types) {
   for (let i = 0; i < cellHeight; i++) {
     for (let j = 0; j < cellWidth; j++) {
       const aliveCellNum = calcAliveCellNum(i, j)
@@ -60,7 +84,10 @@ export function draw(p: p5Types) {
       }
     }
   }
+  drawCurrentBoard(p)
+}
 
+function drawCurrentBoard(p: p5Types) {
   p.clear()
 
   for (let i = 0; i < cellHeight; i++) {
@@ -71,10 +98,43 @@ export function draw(p: p5Types) {
       p.rect(tx, ty, cellSize, cellSize)
     }
   }
-
   board = drawBoard.map((v) => [...v])
 }
 
-export function mouseClicked() {
+function toggleSpeed() {
   speed ^= 1
+}
+
+function stop() {
+  pause = 1
+}
+
+function start() {
+  pause = 0
+}
+
+function initBoard(p: p5Types) {
+  cellHeight = p.round(p.height / cellSize)
+  cellWidth = p.round(p.width / cellSize)
+  board = new Array(cellHeight).fill(0).map(() => new Array(cellWidth).fill(0))
+  drawBoard = board.map((v) => [...v])
+}
+
+function randomBoard(p: p5Types) {
+  initBoard(p)
+  for (let i = 0; i < cellHeight; i++) {
+    for (let j = 0; j < cellWidth; j++) {
+      const t = p.random(100) > 50 ? CellType.alive : CellType.dead
+      board[i][j] = drawBoard[i][j] = t
+    }
+  }
+}
+
+export function mouseClicked(p: p5Types) {
+  const x = p.floor(p.mouseX / cellSize)
+  const y = p.floor(p.mouseY / cellSize)
+
+  drawBoard[y][x] =
+    drawBoard[y][x] === CellType.dead ? CellType.alive : CellType.dead
+  drawCurrentBoard(p)
 }
