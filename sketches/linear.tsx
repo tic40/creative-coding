280a9101c58458b22a3312564a2cade3056a81d2
tic40 <ccpzjoh@gmail.com>
2022-05-01 01:24:41 +0900

    Update

--- sketches/linear.tsx
+++ sketches/linear.tsx
@@ -13,11 +13,10 @@ export function draw(p: p5Types) {
   p.clear()
 
   const t = (p.frameCount % 100) / 100
-  if (t === 0) [start, goal] = [goal, start]
-
-  const x = p.lerp(start, goal, t)
-  p.circle(x, p.height / 2, 30)
-
   p.rect(10, p.height / 2 - 50, 10, 100)
   p.rect(p.width - 20, p.height / 2 - 50, 10, 100)
+
+  if (t === 0) [start, goal] = [goal, start]
+  const x = p.lerp(start, goal, t)
+  p.circle(x, p.height / 2, 30)
 }
