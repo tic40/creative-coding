e93507ebbdbb299328f857e9dcc438a795794875
tic40 <ccpzjoh@gmail.com>
2022-05-01 01:22:34 +0900

    Update

--- sketches/linear.tsx
+++ sketches/linear.tsx
@@ -1,45 +1,23 @@
 import p5Types from 'p5'
 
-interface Route {
-  x: number
-  y: number
-}
-
-let route: Route[]
-let t = 0
-let i = 0
+let start: number
+let goal: number
 
 export function setup(p: p5Types, canvasParentRef: Element) {
   p.createCanvas(p.windowWidth, p.windowHeight).parent(canvasParentRef)
-  const centerX = p.windowWidth / 5
-  const centerY = p.windowHeight / 5
-  route = [
-    { x: p.windowWidth / 5, y: p.windowHeight / 5 },
-    { x: (p.windowWidth / 5) * 4, y: (p.windowHeight / 5) * 4 },
-    { x: (p.windowWidth / 5) * 4, y: p.windowHeight / 5 },
-    { x: p.windowWidth / 5, y: (p.windowHeight / 5) * 4 },
-  ]
+  start = 30
+  goal = p.width - 30
 }
 
 export function draw(p: p5Types) {
   p.clear()
-  p.fill('#FF0000')
-  p.noStroke()
-  for (const { x, y } of route) {
-    p.circle(x, y, 30)
-  }
-  const prev = route[i]
-  const next = route[(i + 1) % route.length]
 
-  const x = p.lerp(prev.x, next.x, t)
-  const y = p.lerp(prev.y, next.y, t)
+  const t = (p.frameCount % 100) / 100
+  if (t === 0) [start, goal] = [goal, start]
 
-  p.circle(x, y, 30)
+  const x = p.lerp(start, goal, t)
+  p.circle(x, p.height / 2, 30)
 
-  t += 0.01
-  if (t > 1) {
-    t = 0
-    i++
-    i %= route.length
-  }
+  p.rect(10, p.height / 2 - 50, 10, 100)
+  p.rect(p.width - 20, p.height / 2 - 50, 10, 100)
 }
