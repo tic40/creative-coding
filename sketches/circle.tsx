cc80a7291e4d6dfe5bca8d2aac1c727764693534
tic40 <ccpzjoh@gmail.com>
2022-05-01 00:51:54 +0900

    Update

--- sketches/circle.tsx
+++ sketches/circle.tsx
@@ -1,10 +1,15 @@
 import p5Types from 'p5'
 
+let circles: { x: number; y: number; size: number }[]
 export function setup(p: p5Types, canvasParentRef: Element) {
+  circles = []
   p.createCanvas(p.windowWidth, p.windowHeight).parent(canvasParentRef)
 }
 
 export function draw(p: p5Types) {
   p.clear()
-  p.circle(p.mouseX, p.mouseY, 100)
+  circles.push({ x: p.mouseX, y: p.mouseY, size: 10 })
+  circles = circles.slice(-500)
+  for (const v of circles) v.size += 0.5
+  for (const v of circles) p.circle(v.x, v.y, v.size)
 }
--- sketches/circle_random.tsx
+++ sketches/circle_random.tsx
@@ -1,9 +1,19 @@
 import p5Types from 'p5'
 
+let circles: { x: number; y: number; size: number }[]
 export function setup(p: p5Types, canvasParentRef: Element) {
+  circles = []
   p.createCanvas(p.windowWidth, p.windowHeight).parent(canvasParentRef)
 }
 
 export function draw(p: p5Types) {
-  p.circle(p.mouseX, p.mouseY, p.random(100))
+  p.clear()
+  circles.push({
+    x: p.random(p.windowWidth),
+    y: p.random(p.windowHeight),
+    size: p.random(10, 100),
+  })
+  for (const v of circles) v.size -= 0.5
+  circles = circles.filter((v) => 0 < v.size)
+  for (const v of circles) p.circle(v.x, v.y, v.size)
 }
