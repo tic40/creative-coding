fad4365a7bae292bd2cc9d69303a85a583ec43dd
tic40 <ccpzjoh@gmail.com>
2022-05-03 17:37:41 +0900

    Rename

---
+++ pages/sketches/index.tsx
@@ -0,0 +1,49 @@
+import type { NextPage } from 'next'
+import getConfig from 'next/config'
+import Link from 'next/link'
+import Meta from '../../components/meta'
+
+const Home: NextPage = () => {
+  const {
+    publicRuntimeConfig: { sketchFileNames },
+  } = getConfig()
+  return (
+    <>
+      <Meta
+        title={process.env.siteName || ''}
+        description={process.env.siteName || ''}
+      />
+      <main className="m-4">
+        <h1 className="text-4xl">
+          <Link href="/">
+            <a>{process.env.siteName}</a>
+          </Link>
+        </h1>
+        <div className="my-5 flex-wrap flex gap-3">
+          {sketchFileNames.map((name: string) => {
+            return (
+              <div key={`sketch-${name}`}>
+                <Link href={`/sketches/${name}`}>
+                  <a className="bg-transparent text-blue-700 py-1 px-3 border border-blue-500 rounded">
+                    {name}
+                  </a>
+                </Link>
+              </div>
+            )
+          })}
+        </div>
+        <footer>
+          <a
+            href="https://github.com/tic40/creative-coding"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
+      </main>
+    </>
+  )
+}
+
+export default Home
--- sketches/line1.tsx
+++ sketches/line1.tsx
@@ -6,7 +6,7 @@ interface Rgb {
   b: number
 }
 
-class Ball {
+class Point {
   x: number
   y: number
   vx: number
@@ -46,35 +46,34 @@ class Ball {
     return new this(
       p.random(0, p.width),
       p.random(0, p.height),
-      p.random(-3, 3),
-      p.random(-3, 3),
+      p.random(-5, 5),
+      p.random(-5, 5),
       p
     )
   }
 }
 
-let a: Ball
-let b: Ball
-let c: Rgb
-let darkMode = false
+let a: Point
+let b: Point
+let color: Rgb
 
 function randomColor(p: p5Types) {
-  c.r += p.random([-2, 2])
-  c.g += p.random([-2, 2])
-  c.b += p.random([-2, 2])
+  color.r += p.random([-2, 2])
+  color.g += p.random([-2, 2])
+  color.b += p.random([-2, 2])
 
-  c.r = p.constrain(c.r, 0, 255)
-  c.g = p.constrain(c.g, 0, 255)
-  c.b = p.constrain(c.b, 0, 255)
+  color.r = p.constrain(color.r, 0, 255)
+  color.g = p.constrain(color.g, 0, 255)
+  color.b = p.constrain(color.b, 0, 255)
 }
 
 export function setup(p: p5Types, canvasParentRef: Element) {
   p.createCanvas(p.windowWidth, p.windowHeight).parent(canvasParentRef)
 
   p.noFill()
-  a = Ball.createRandom(p)
-  b = Ball.createRandom(p)
-  c = { r: p.random(255), g: p.random(255), b: p.random(255) }
+  a = Point.createRandom(p)
+  b = Point.createRandom(p)
+  color = { r: p.random(255), g: p.random(255), b: p.random(255) }
 }
 
 export function draw(p: p5Types) {
@@ -82,13 +81,6 @@ export function draw(p: p5Types) {
   b.update()
 
   randomColor(p)
-  p.stroke(c.r, c.g, c.b, 100)
+  p.stroke(color.r, color.g, color.b, 100)
   p.line(a.x, a.y, b.x, b.y)
-}
-
-export function mouseClicked(p: p5Types) {
-  darkMode = !darkMode
-  p.clear()
-  if (darkMode) p.background(180)
-  else p.background(255)
 }
