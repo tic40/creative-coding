d6298d7a0fb9ff5967c4485fc51a53aae10a9d17
tic40 <ccpzjoh@gmail.com>
2024-07-02 09:34:12 +0900

    Format

--- sketches/sierpinski_carpet.tsx
+++ sketches/sierpinski_carpet.tsx
@@ -1,17 +1,22 @@
 import p5Types from 'p5'
 
-type Pattern = string[][];
+type Pattern = string[][]
 
 function carpet(n: number): Pattern {
-  if (n == 0) return [["#"]]
-  const p = carpet(n-1)
+  if (n == 0) return [['#']]
+  const p = carpet(n - 1)
   const psize = p.length
-  const res: Pattern = Array.from({ length: psize * 3 }, () => ".".repeat(psize * 3).split(''));
-  for(let i = 0; i < 3; i++) for(let j = 0; j < 3; j++) if (i != 1 || j != 1) {
-    for(let x = 0; x < psize; x++) for(let y = 0; y < psize; y++) {
-      res[psize * i + x][psize * j + y] = p[x][y]
-    }
-  }
+  const res: Pattern = Array.from({ length: psize * 3 }, () =>
+    '.'.repeat(psize * 3).split('')
+  )
+  for (let i = 0; i < 3; i++)
+    for (let j = 0; j < 3; j++)
+      if (i != 1 || j != 1) {
+        for (let x = 0; x < psize; x++)
+          for (let y = 0; y < psize; y++) {
+            res[psize * i + x][psize * j + y] = p[x][y]
+          }
+      }
   return res
 }
 
@@ -27,11 +32,16 @@ export function setup(p: p5Types, canvasParentRef: Element) {
 export function draw(p: p5Types) {
   p.noStroke()
   p.noFill()
-  for(let i = 0; i < 40; i++) {
+  for (let i = 0; i < 40; i++) {
     if (Math.floor(t / data.length) >= data.length) return
     const now = data[Math.floor(t / data.length)][t % data.length]
     now == '#' ? p.fill(0) : p.noFill()
-    p.rect((t % data.length) * size, Math.floor(t/data.length) * size,size,size)
+    p.rect(
+      (t % data.length) * size,
+      Math.floor(t / data.length) * size,
+      size,
+      size
+    )
     t++
   }
 }
